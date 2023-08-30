7887b80fc6f74ab1fc9ab403af701502a5045715
Joás <cabraljrjoas@tutanota.com>
2023-08-29 23:23:03 -0300

    feat: the little `entity`

---
+++ src/main/java/dev/maltexto/boh/entity/Fornecedor.java
@@ -0,0 +1,31 @@
+package dev.maltexto.boh.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "fornecedor")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cpnj;
+
+    private String nome;
+
+    private String email;
+
+    private String comentario;
+
+}
