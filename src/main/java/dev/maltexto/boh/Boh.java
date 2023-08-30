7054270e68a601cd8ed16c6f24eeda418307e826
Joás <cabraljrjoas@tutanota.com>
2023-08-29 23:19:41 -0300

    feat: add basic `controller`

---
+++ src/main/java/dev/maltexto/boh/Boh.java
@@ -0,0 +1,9 @@
+package dev.maltexto.boh;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/boh")
+public class Boh extends Application {
+
+}
---
+++ src/main/java/dev/maltexto/boh/controller/FornecedoresController.java
@@ -0,0 +1,66 @@
+package dev.maltexto.boh.controller;
+
+import dev.maltexto.boh.entity.Fornecedor;
+import dev.maltexto.boh.service.FornecedorService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/fornecedores")
+public class FornecedoresController {
+
+    @Inject
+    private FornecedorService fornecedorService;
+
+    @GET
+    @Path("/ping")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response ping() {
+        return Response.ok("Pong").build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findAll() {
+        return Response.ok(fornecedorService.findAll()).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findOneById(@PathParam("id") Long id) {
+        return Response.ok(fornecedorService.findById(id)).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Fornecedor fornecedor) {
+        return Response
+                .status(Response.Status.CREATED)
+                .entity(fornecedorService.save(fornecedor))
+                .build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("id") Long id, Fornecedor fornecedor) {
+        return Response.ok(fornecedorService.update(id, fornecedor)).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response delete(@PathParam("id") Long id) {
+        return Response.ok(fornecedorService.delete(id)).build();
+    }
+}
