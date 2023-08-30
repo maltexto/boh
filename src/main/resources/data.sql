2ef8746ff9feb018685e338e26b7916c25bcd6e6
Joás <cabraljrjoas@tutanota.com>
2023-08-29 23:31:26 -0300

    feat: `data.sql` to populate database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO fornecedor (cnpj, nome, email, comentario) VALUES
+  ('11111111111111', 'Fornecedor A', 'fornecedorA@example.com', 'Comentário para Fornecedor A'),
+  ('22222222222222', 'Fornecedor B', 'fornecedorB@example.com', 'Comentário para Fornecedor B'),
+  ('33333333333333', 'Fornecedor C', 'fornecedorC@example.com', 'Comentário para Fornecedor C');
